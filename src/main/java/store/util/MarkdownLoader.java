8ae4ad363cad561f7d99644538182abd538b6df9
JunBe <wnsqowid0901@gmail.com>
2024-11-06 21:31:18 +0900

    feat: 마크다운 폴더 로드 기능 추가
    마크다운 파일의 내용을 불러오기 위한 클래스를 작성했다.

---
+++ src/main/java/store/util/MarkdownLoader.java
@@ -0,0 +1,24 @@
+package store.util;
+
+import java.io.*;
+
+public class MarkdownLoader {
+    public static String loadMarkdown(String fileName) {
+        StringBuilder content = new StringBuilder();
+        readLine(fileName, content);
+        return content.toString();
+    }
+
+    private static void readLine(String fileName, StringBuilder content) {
+        try (InputStream inputStream = MarkdownLoader.class.getClassLoader().getResourceAsStream(fileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
